8f9aa5601802c994523face0da8e1e75e71a7f00
lacerbi <luigi.acerbi@gmail.com>
2024-11-24 12:14:38 +0200

    refactor: use Create React App (react-scripts)

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import InteractiveVI from './components/InteractiveVI';
+
+function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen p-4 bg-white">
+      <InteractiveVI />
+    </div>
+  );
+}
+
+export default App;
